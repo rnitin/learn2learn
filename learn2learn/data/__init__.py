0f54abac4973812b50b344475c5e95c260de80a8
nitin <alekhya.ramarao@gmail.com>
2022-05-06 12:48:59 -0500

    fixing issues in data __init__ and adding particles2d

--- learn2learn/data/__init__.py
+++ learn2learn/data/__init__.py
@@ -4,7 +4,7 @@ r"""
 A set of utilities for data & tasks loading, preprocessing, and sampling.
 """
 
-from learn2learn.optim import transforms
-from learn2learn.data.meta_dataset import MetaDataset, UnionMetaDataset, FilteredMetaDataset
-from learn2learn.data.task_dataset import TaskDataset, DataDescription
+from . import transforms
+from .meta_dataset import MetaDataset, UnionMetaDataset, FilteredMetaDataset
+from .task_dataset import TaskDataset, DataDescription
 from .utils import OnDeviceDataset, partition_task, InfiniteIterator
